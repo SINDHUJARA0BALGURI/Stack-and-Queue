de838c5f5c4aa749d12fa59606db3fabb86d5c62
SINDHUJARA0BALGURI <84570484+SINDHUJARA0BALGURI@users.noreply.github.com>
2021-09-02 17:07:52 +0530

    Enqueueing of stack is done

--- Assets/QueueExample.cs
+++ Assets/QueueExample.cs
@@ -5,15 +5,30 @@ using TMPro;
 
 public class QueueExample : MonoBehaviour
 {
+    [SerializeField]
+    private TextMeshProUGUI queueDebug;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        Queue queue = new Queue();
 
-    // Update is called once per frame
-    void Update()
+        queue.Enqueue("S");
+        queue.Enqueue("I");
+        queue.Enqueue("N");
+        queue.Enqueue("D");
+        queue.Enqueue("H");
+        queue.Enqueue("U");
+
+        ShowInformationInQueue(queue);
+
+    }
+    public void ShowInformationInQueue(Queue queue)
     {
-        
+        queueDebug.text += "...............\n";
+        foreach(var queueitem in queue)
+        {
+            queueDebug.text += "{queueitem}\n";
+        }
     }
 }
